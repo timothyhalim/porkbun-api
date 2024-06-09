060e7be90596886ce34eced5efb197658b836a28
Timothy Halim <timothy.halim@live.com>
2024-06-09 12:08:02 +0700

    Move logger insed PorkBunAPI class

--- api.py
+++ api.py
@@ -5,15 +5,6 @@ from enum import Enum
 from urllib import request
 from urllib.error import HTTPError
 
-LOGGER = logging.getLogger("PorkBunAPI")
-LOGGER.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-for handler, level in ((logging.FileHandler("porkbun.log"), logging.DEBUG),
-                       (logging.StreamHandler(), logging.INFO)):
-    handler.setLevel(level)
-    handler.setFormatter(formatter)
-    LOGGER.addHandler(handler)
-
 class Mode(Enum):
     CREATE = "create"
     READALL = "retrieve"
@@ -25,8 +16,16 @@ class Mode(Enum):
         return self.value
     
 class PorkBunAPI():
-    def __init__(self, config_file="config.json", domain=""):
-        LOGGER.info(f"{domain} initialized using {os.path.abspath(config_file)}")
+    def __init__(self, domain="", config_file="config.json", log_file="porkbun.log"):
+        self.logger = logging.getLogger("PorkBunAPI")
+        self.logger.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        for handler, level in ((logging.FileHandler(log_file), logging.DEBUG),
+                               (logging.StreamHandler(), logging.INFO)):
+            handler.setLevel(level)
+            handler.setFormatter(formatter)
+            self.logger.addHandler(handler)
+        self.logger.info(f"{domain} initialized using {os.path.abspath(config_file)}")
         self.domain = domain
         with open(config_file) as f:
             self.config = json.load(f)
@@ -38,9 +37,9 @@ class PorkBunAPI():
         try:
             response = request.urlopen(req, timeout=10).read()
         except HTTPError as err:
-            LOGGER.error(f"{str(err)}")
-            LOGGER.error(f"Url : {err.url}")
-            LOGGER.error(f"Data: {str(data)}")
+            self.logger.error(f"{str(err)}")
+            self.logger.error(f"Url : {err.url}")
+            self.logger.error(f"Data: {str(data)}")
             raise err
         return json.loads(response.decode("utf-8"))
             
@@ -48,7 +47,7 @@ class PorkBunAPI():
         return self._api("ping")["yourIp"]
     
     def get_records(self):
-        LOGGER.info(f"Getting records for {self.domain}")
+        self.logger.info(f"Getting records for {self.domain}")
         target = f'dns/{Mode.READALL}/{self.domain}'
         return self._api(target)["records"]
     
@@ -61,7 +60,7 @@ class PorkBunAPI():
             return self.create_record(type, name, content, ttl)
         
     def create_record(self, type, name, content, ttl):
-        LOGGER.info(f"Creating {type} record for {name+'.' if name else ''}{self.domain} pointing to {content}")
+        self.logger.info(f"Creating {type} record for {name+'.' if name else ''}{self.domain} pointing to {content}")
         target = f'dns/{Mode.CREATE}/{self.domain}'
         return self._api(target=target, data={
             "apikey": self.config["apikey"],
@@ -73,13 +72,13 @@ class PorkBunAPI():
         })
         
     def read_record(self, type, name):
-        LOGGER.info(f"Reading {type} record of {name+'.' if name else ''}{self.domain}")
+        self.logger.info(f"Reading {type} record of {name+'.' if name else ''}{self.domain}")
         target = f'dns/{Mode.READ}/{self.domain}/{type}/{name}'
         records = self._api(target=target)["records"]
         return records[0] if records else None
         
     def update_record(self, type, name, content, ttl):
-        LOGGER.info(f"Updating {type} record of {name+'.' if name else ''}{self.domain} pointing to {content}")
+        self.logger.info(f"Updating {type} record of {name+'.' if name else ''}{self.domain} pointing to {content}")
         target = f'dns/{Mode.UPDATE}/{self.domain}/{type}/{name}'
         return self._api(target=target, data={
             "apikey": self.config["apikey"],
@@ -89,6 +88,6 @@ class PorkBunAPI():
         })
         
     def delete_record(self, type, name):
-        LOGGER.info(f"Deleting {type} record from {name+'.' if name else ''}{self.domain}")
+        self.logger.info(f"Deleting {type} record from {name+'.' if name else ''}{self.domain}")
         target = f'dns/{Mode.DELETE}/{self.domain}/{type}/{name}'
         return self._api(target=target)
